3ba5ae64ff2a7c4af97f1c2354a91606fe5361b2
AdM <96199796+A3d0M3@users.noreply.github.com>
2023-12-09 14:38:15 +0200

    Add files via upload

---
+++ DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+namespace DbConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("DbConnectionString");
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+
+            string query = "DELETE FROM author WHERE id > 8";
+
+            MySqlCommand command = new MySqlCommand(query, connection);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
